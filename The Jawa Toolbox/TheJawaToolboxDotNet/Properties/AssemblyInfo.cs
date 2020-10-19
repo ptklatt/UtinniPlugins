bf3ffd48fb9d5c54de37d68f6bf8747bb83652af
ptklatt <timbab2@gmail.com>
2020-10-20 00:29:30 +0200

    Added basic versioning to both projects

---
+++ The Jawa Toolbox/TheJawaToolboxDotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TheJawaToolboxDotNet")]
+[assembly: AssemblyDescription("The Jawa Toolbox .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Philip Klatt")]
+[assembly: AssemblyProduct("TheJawaToolboxDotNet")]
+[assembly: AssemblyCopyright("Copyright ©  2020, Philip Klatt")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29bc7f19-ca68-4712-97c8-ee5b4f9fc738")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
